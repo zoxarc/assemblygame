d7910556599fd294fac243c2b55dc2c5a0815ef4
zoxarc <zoxarc@gmail.com>
2022-04-13 10:56:41 +0300

    added the player character

--- compile.bat
+++ compile.bat
@@ -1,4 +1,3 @@
 set tar=project
 tasm.exe /zi %tar%.asm
 tlink.exe /v %tar%.obj
-project.exe
--- project.asm
+++ project.asm
@@ -7,66 +7,80 @@ wall dw ?
 pcor dw  ?  ; coordinates for player
 pcorbackup dw ? ; Backup of pcor
 laser dw ?
-maxhealth dw 3
-health dw 3
+maxhealth dw 3  ;the max amount of health the player can have
+health dw 3     ;how much health the player has now
 pcolor dw 4
-pdir dw ?
+pdir dw 3       ;the direction of the player
 dtimer dw 0
 FARDATA bufferseg ;the buffer
 buffer db 64000 dup(12h)
 
+
+;r	project.asm	/^$/;"	l 
+
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ;           macros
 
-macro callpixel p1,p2
+macro callrect p1,p2
 push ax
 push di
 push p1
 push p2
-call pixeltobuffer
-pop di
-pop ax
-endm callpixel
-
-macro callrect p1,p2
-push ax
-push p1
-push p2
 call drawrect
+pop di
 pop ax
 endm callrect
 
 macro pixel p1,p2
 push ax
+push cx
 push bx
 push p1
 push p2
 call rect2x2
 pop bx
+pop cx
 pop ax
 endm pixel
 
 macro calc p1,p2,p3
 push ax
+push cx
 push bx
 push offset p1
 push p2
 push p3
 call coordinatecalc
 pop bx
+pop cx
 pop ax
 endm calc
 
-macro callplayer p1,p2,p3
+macro callplayer p1
 push ax
+push cx
+push di
+push bx
+push p1
+push [pdir]
+call player
+pop bx
+pop di
+pop cx
+pop ax
+endm callplayer
+
+macro pclear p1,p2
+push ax
+push di
 push bx
 push p1
 push p2
-push p3
-call drawplayer
+call clearplayer
 pop bx
+pop di
 pop ax
-endm callplayer
+endm pclear
 
 macro life
 push ax
@@ -82,15 +96,6 @@ endm life
 
 CODESEG
 ;            procedures
-proc pixeltobuffer   ;First push y second x third color
-mov bp,sp            ;Freeze the stack pointer
-xor ax,ax            ;Reset ax and di
-xor di,di
-mov di,[bp+4]        ;di = calculated coordinates 
-mov al,[bp+2]        ;al = colour
-mov [es:di],al       ;Store colour in buffer at (x,y)
-ret 4
-endp pixeltobuffer
 
 ;Copy everything in the buffer to display memory
 proc buffertoscreen    
@@ -111,72 +116,68 @@ mov es,ax              ;es = segment for buffer
 ret
 endp buffertoscreen
 
-;draw a rectangle using the coordinate as the top left pixel
-proc drawrect 
+;draw a rectangle using the coordinate as the top left pixel - currently broken
+proc drawrect
 mov bp,sp ;Freeze the stack pointer
 xor ax,ax ;Set ax,bx,si,di to 0
-xor bx,bx
-xor si,si
+xor cx,cx
 xor di,di
-mov ax,[bp+4] ;ax = coordinate
-mov bx,[bp+2] ;bx = color
-sub ax,320    ;Subtract from ax 317 to neutrlize the first add and inc
-add ax,3
-mov di,4
+mov di,[bp+4] ;di = coordinate
+mov al,[bp+2] ;al = color
+sub di,317    ;Subtract from ax 317 to neutrlize the first add and inc
+mov cl,4
 @outerrectloop:
-add ax,320
-sub ax,4
-mov si,4
+add di,320
+sub di,4
+mov ch,4
 @innerrectloop:
-inc ax
-callpixel ax bx 
-dec si
+inc di
+mov [byte ptr es:di],al ;move al into the buffer
+dec ch
 jnz @innerrectloop
-dec di
+dec cl
 jnz @outerrectloop
 ret 4
 endp drawrect
 
 proc rect2x2
 mov bp,sp ;Freeze the stack pointer
-xor ax,ax ;Set ax,bx,si,di to 0
-xor bx,bx
-xor si,si
+xor ax,ax ;Set di,ax,cx to 0
+xor cx,cx
 xor di,di
-mov ax,[bp+4] ;ax = coordinate
-mov bx,[bp+2] ;bx = color
-sub ax,320    ;Subtract from ax 317 to neutrlize the first add and inc
-add ax,1
-mov di,2
+mov di,[bp+4] ;di = coordinate
+mov al,[bp+2] ;al = color
+sub di,319    ;Subtract from di 319 to neutrlize the first add and inc
+mov cl,2
 @outerrectloop1:
-add ax,320
-sub ax,2
-mov si,2
+add di,318
+mov ch,2
 @innerrectloop1:
-inc ax
-callpixel ax bx 
-dec si
+inc di
+mov [es:di],al
+dec ch
 jnz @innerrectloop1
-dec di
+dec cl
 jnz @outerrectloop1
 ret 4
 endp rect2x2
 
+;procedure to draw the player lives on screen
 proc lives
 mov bp,sp
 mov ax,[bp+2]
 mov bx,[bp+4]
-calc wall 10 10
-cmp bx,ax
+calc wall 10 10  ;calc the coordinates of the hearts
+cmp bx,ax   ;check if player has max health
 je @redlives
 mov cx,[wall]
 @emptylives:
-pixel cx 15
+pixel cx 15 ; draw max health of empty hearts
 add cx,5
 dec bx
 jnz @emptylives
 @redlives:
-pixel [wall] 4
+pixel [wall] 4 ; draw health amount of red hearts
 add [wall],5
 dec ax
 jnz @redlives
@@ -201,48 +202,256 @@ pixel [pcor] 28h
 ret
 endp hitdetected
 
-; 0 is left, 1 is up, 2 is right, 3 is down
-proc direction
-mov bp,sp
-mov cx,[bp+2]
-mov ax,[bp+4]
-cmp cx,2
-jcxz @leftcp
-ja @dirdown
-je @dirright
-jb @dirup
-@leftcp:
-jmp @dirleft
-@dirdown:
-add ax,320
-callpixel ax 0Ah
-inc ax
-callpixel ax 0Ah
-jmp @dirend
-@dirright:
-inc ax
-callpixel ax 0Ah
-add ax,320
-callpixel ax 0Ah
-jmp @dirend
-@dirup:
-callpixel ax 0Ah
-inc ax
-callpixel ax 0Ah
-jmp @dirend
-@dirleft:
-callpixel ax 0Ah
-add ax,320
-callpixel ax 0Ah
 
-@dirend:
+
+; draw the player character, each proc draws the player from a different side
+proc drawfrontplayer
+mov bp,sp
+mov di,[bp+2]
+mov cx,3
+@fronthair:
+inc di
+mov [byte ptr es:di],6h
+dec cx
+jnz @fronthair
+add di,318
+mov [byte ptr es:di],28h
+inc di
+mov [byte ptr es:di],0Fh
+inc di
+mov [byte ptr es:di],28h
+add di,317
+mov cx,3
+@frontface:
+inc di
+mov [byte ptr es:di],42h
+dec cx
+jnz @frontface
+add di,317
+mov [byte ptr es:di],77h
+mov [byte ptr es:di+4],77h
+mov ch,2
+mov [byte ptr es:di+324],43h
+mov [byte ptr es:di+320],43h
+
+@frontshirt1:
+mov cl,3
+@frontshirt2:
+inc di
+mov [byte ptr es:di],02h
+dec cl
+jnz @frontshirt2
+add di,317
+dec ch
+jnz @frontshirt1
+inc di
+mov [byte ptr es:di],20h
+mov [byte ptr es:di+1],20h
+mov [byte ptr es:di+2],01h
+add di,320
+mov [byte ptr es:di],00h
+mov [byte ptr es:di+2],00h
+
+ret 2
+endp drawfrontplayer
+
+proc drawbackplayer
+mov bp,sp
+mov di,[bp+2]
+mov ch,2
+@backhair1:
+mov cl,3
+@backhair2:
+inc di
+mov [byte ptr es:di],6h
+dec cl
+jnz @backhair2
+add di,317
+dec ch
+jnz @backhair1
+mov cl,3
+@scalp:
+inc di
+mov [byte ptr es:di],43h
+dec cl
+jnz @scalp
+add di,317
+mov [byte ptr es:di],77h
+mov [byte ptr es:di+4],02h
+mov [byte ptr es:di+320],43h
+mov [byte ptr es:di+324],43h
+mov ch,2
+@backshirt1:
+mov cl,3
+@backshirt2:
+inc di
+mov [byte ptr es:di],02h
+dec cl
+jnz @backshirt2
+add di,317
+dec ch
+jnz @backshirt1
+inc di
+mov [byte ptr es:di],20h
+mov [byte ptr es:di+1],20h
+mov [byte ptr es:di+2],01h
+add di,320
+mov [byte ptr es:di],00h
+mov [byte ptr es:di+2],00h
+
+ret 2
+endp drawbackplayer
+
+proc drawrightplayer
+mov bp,sp
+mov di,[bp+2]
+mov cl,3
+@righthair:
+inc di
+mov [byte ptr es:di],6h
+dec cl
+jnz @righthair
+add di,318
+mov [byte ptr es:di],6h
+inc di
+mov [byte ptr es:di],60h
+inc di
+mov [byte ptr es:di],4h
+add di,317
+mov cl,3
+@rightface:
+inc di
+mov [byte ptr es:di],42h
+dec cl
+jnz @rightface
+mov cl,3
+add di,317
+@rightshirt:
+inc di
+mov [byte ptr es:di],02h
+dec cl
+jnz @rightshirt
+add di,318
+mov [byte ptr es:di],02h
+inc di
+mov [byte ptr es:di],77h
+inc di
+mov [byte ptr es:di],77h
+inc di
+mov [byte ptr es:di],42h
+add di,317
+mov [byte ptr es:di],01h
+inc di
+mov [byte ptr es:di],20h
+inc di
+mov [byte ptr es:di],20h
+add di,318
+mov [byte ptr es:di],00h
+mov [byte ptr es:di+2],00h
+
+ret 2
+endp drawrightplayer
+
+proc drawleftplayer
+mov bp,sp
+mov di,[bp+2]
+mov cl,3
+@lefthair:
+inc di
+mov [byte ptr es:di],6h
+dec cl
+jnz @lefthair
+add di,318
+mov [byte ptr es:di],4h
+inc di
+mov [byte ptr es:di],60h
+inc di
+mov [byte ptr es:di],6h
+add di,317
+mov cl,3
+@leftface:
+inc di
+mov [byte ptr es:di],42h
+dec cl
+jnz @leftface
+mov cl,3
+add di,317
+@leftshirt:
+inc di
+mov [byte ptr es:di],02h
+dec cl
+jnz @leftshirt
+add di,317
+mov [byte ptr es:di],42h
+inc di
+mov [byte ptr es:di],77h
+inc di
+mov [byte ptr es:di],77h
+inc di
+mov [byte ptr es:di],02h
+add di,318
+mov [byte ptr es:di],20h
+inc di
+mov [byte ptr es:di],20h
+inc di
+mov [byte ptr es:di],01h
+add di,318
+mov [byte ptr es:di],00h
+mov [byte ptr es:di+2],00h
+
+ret 2
+endp drawleftplayer
+
+proc player
+mov bp,sp
+push [bp+4]
+mov cx,[bp+2] ;cx is the direction of the player
+cmp cx,2  ; 0 is left, 1 is up, 2 is right, 3 is down
+jcxz @pdirleft
+jg @pdirdown
+je @pdirright
+jb @pdirup
+@pdirleft:
+call drawleftplayer
+jmp @pdend
+@pdirdown:
+call drawfrontplayer
+jmp @pdend
+@pdirright:
+call drawrightplayer
+jmp @pdend
+@pdirup:
+call drawbackplayer
+jmp @pdend
+
+@pdend:
 ret 4
-endp direction
+endp player
+
+;player is 7x5
+proc clearplayer
+mov bp,sp
+mov di,[bp+4]
+mov al,[bp+2]
+mov ch,8
+@clearloop1:
+mov cl,8
+@clearloop2:
+mov [byte ptr es:di],al
+inc di
+dec cl
+jnz @clearloop2
+add di,312
+dec ch
+jnz @clearloop1
+
+ret 4
+endp clearplayer
 
 proc lvl1
-calc pcor 100 160
-pixel [pcor] [pcolor]
 life
+calc pcor 100 160
+callplayer [pcor]
 calc wall 20 20
 mov cx,280
 lvl1loop1:
@@ -295,7 +504,8 @@ int 10h              ;switch to mode 13h
 call lvl1            ;generate level 1
 
 calc laser 100 100
-pixel [laser] 9
+;push [laser]
+;call drawleftplayer
 
 
 @waitforkey:
@@ -305,7 +515,7 @@ int 16h             ;Check if there's input in the buffer
 jz @waitforkey
 mov ah,0           
 int 16h             ;Get the input and clean the buffer 
-pixel [pcor] 12h 
+pclear [pcor] 12h 
 mov bx,[pcor]
 mov [pcorbackup],bx ;save the current location of the player in case there's a collision
 cmp ah,11h          ;find if the input matches w,a,s,d,esc
@@ -321,7 +531,7 @@ je @interact
 
 cmp ah,1h           
 je @exitcp
-pixel [pcor] [pcolor] 
+callplayer [pcor]
 jmp @waitforkey
 
 @wpressed:
@@ -348,6 +558,13 @@ jmp @collisioncheck
 jmp exit           ;checkpoint for exit
 
 @interact:
+mov di,[pcor]
+inc di
+cmp [byte ptr es:di],06h
+je @lever
+@lever:
+
+
 
 @collisioncheck:
 mov di,[pcor]
@@ -394,10 +611,7 @@ jne @collisionfound
 
 
 @moveplayer:
-pixel [pcor] [pcolor] 
-push [pcor]
-push [pdir]
-call direction
+callplayer [pcor]
 jmp @waitforkey
 
 @collisionfound:
@@ -406,7 +620,6 @@ jne @stopmovement
 dec [health]
 jz exit
 life
-mov [pcolor],02h
 @stopmovement:
 mov ax,[pcorbackup]
 mov [pcor],ax
